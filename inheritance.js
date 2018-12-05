b6b3d78fb1ac5fa25a6867d834ac317d3bcac4a1
Swordo <swordo95@icloud.com>
2018-12-05 18:53:10 +0530

    done

---
+++ inheritance.js
@@ -0,0 +1,17 @@
+// inheritance with Function prototype
+function Square() {
+    this.h = 10;
+    this.w = 20;
+
+}
+
+function S() {
+    Square.call(this);
+
+}
+S.prototype.disp = function () {
+    console.log(this.w * this.h);
+}
+
+let obj = new S();
+obj.disp();
